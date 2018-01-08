c35d0114468e4b52bbb1f9433c0c49656a0f8e0c
Habib <hbrawnak@gmail.com>
2018-01-08 13:39:20 +0600

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+=begin
   root :to => "resumes#index"
-   resources :resumes
+=end
+   resources :resumes, :path => '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
